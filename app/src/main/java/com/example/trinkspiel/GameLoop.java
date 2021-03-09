724bd011cc552c7213203775bbef2dc1eea3e318
TomEckert00 <toe00@web.de>
2021-03-09 18:02:22 +0100

    fullscreen in gameloop

--- app/src/main/java/com/example/trinkspiel/GameLoop.java
+++ app/src/main/java/com/example/trinkspiel/GameLoop.java
@@ -2,6 +2,7 @@ package com.example.trinkspiel;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +22,7 @@ public class GameLoop extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game_loop);
         aufgabe = findViewById(R.id.AufgabenTextView);
     }
