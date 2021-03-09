58d14dab6cb480ecbb4d274f582546f414455daf
TomEckert00 <toe00@web.de>
2021-03-09 21:30:33 +0100

    card shuffeling and player filling system

--- app/src/main/java/com/example/trinkspiel/GameLoop.java
+++ app/src/main/java/com/example/trinkspiel/GameLoop.java
@@ -1,23 +1,29 @@
 package com.example.trinkspiel;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.trinkspiel.gamepackages.GamePackageStandard;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class GameLoop extends AppCompatActivity {
     TextView aufgabe;
-    GamePackageStandard gamePack = new GamePackageStandard();
+    ConstraintLayout mainLayout;
+    GamePackageStandard gamePack;
     ArrayList<String> cards;
     ArrayList<String> players;
     int cardIndex;
+    boolean touchedRightHalf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,35 +31,92 @@ public class GameLoop extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_game_loop);
         aufgabe = findViewById(R.id.AufgabenTextView);
+        mainLayout = findViewById(R.id.mainLayout);
+        mainLayout.setOnTouchListener(new View.OnTouchListener(){
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int x = (int) event.getX();
+                int screenWidth = FetchDynamicScreenWidth();
+                if(x >= screenWidth/2){
+                    touchedRightHalf = true;
+                }
+                else {
+                    touchedRightHalf = false;
+                }
+                changeCard();
+                return false;
+            }
+        });
     }
+
+    private int FetchDynamicScreenWidth() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        return displayMetrics.widthPixels;
+    }
+
 
     @Override
     protected void onResume() {
         super.onResume();
-        players = GroupPage.getPlayerList();
+        shuffleCardsFillWithPlayersAnSetIndexToZero();
+        changeCard();
+    }
+
+    private void shuffleCardsFillWithPlayersAnSetIndexToZero() {
+        gamePack = new GamePackageStandard();
         cards = gamePack.getCards();
         shuffleInRandomOrder(cards);
+        players = GroupPage.getPlayerList();
+        fillCardsWithPlayers();
         cardIndex = 0;
     }
 
-    public void changeCard(View v){
-        String aktuelleAufgabe = cards.get(cardIndex);
-        String randomPlayer = players.get(getRandomNumber(0,players.size()));
-        aktuelleAufgabe = aktuelleAufgabe.replace("$Sp1", randomPlayer);
-        aufgabe.setText(aktuelleAufgabe);
-        cardIndex++;
-        if (cardIndex == cards.size()){
-            cardIndex = 0;
-            shuffleInRandomOrder(cards);
+    public void changeCard(){
+        if (touchedRightHalf){
+            cardIndex++;
+            if (cardIndex == cards.size()){
+                System.out.println("Letzte Karte erreicht, starte von vorne");
+                shuffleCardsFillWithPlayersAnSetIndexToZero();
+            }
+        }else{
+            cardIndex--;
+            if (cardIndex<0){
+                cardIndex=0;
+            }
         }
+        String aktuelleAufgabe = cards.get(cardIndex);
+        aufgabe.setText(aktuelleAufgabe);
+
     }
 
     private int getRandomNumber(int min, int max) {
         return (int) ((Math.random() * (max - min)) + min);
     }
 
-    private void shuffleInRandomOrder(ArrayList<String> liste){
-        Collections.shuffle(liste);
+    private void shuffleInRandomOrder(ArrayList<String> list){
+        Collections.shuffle(list);
+    }
+
+    private void fillCardsWithPlayers(){
+        ArrayList<String> kartenDeck = cards;
+        ArrayList<String> spieler = players;
+        String removedPlayer = null;
+        int repetitions = cards.size();
+        for(int i=0;i<repetitions;i++){
+            String randomPlayer = spieler.get(getRandomNumber(0,spieler.size()));
+            String TaskWithPlayerReplaced = kartenDeck.get(i).replace("$Sp1", randomPlayer);
+            kartenDeck.set(i, TaskWithPlayerReplaced);
+            spieler.remove(randomPlayer);
+            if(removedPlayer != null){
+                spieler.add(removedPlayer);
+            }
+            removedPlayer = randomPlayer;
+        }
+        spieler.add(removedPlayer);
+        cards = kartenDeck;
+        System.out.println("Neues Deck:");
+        System.out.println(cards.toString());
     }
 
 }
