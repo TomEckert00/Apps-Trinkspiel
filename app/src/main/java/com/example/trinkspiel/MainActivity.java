29cb6a00e9b78cd3fa530b1261ab483743781683
TomEckert00 <toe00@web.de>
2021-03-22 21:29:04 +0100

    internationalisation

--- app/src/main/java/com/example/trinkspiel/MainActivity.java
+++ app/src/main/java/com/example/trinkspiel/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.trinkspiel;
 
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +19,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*
+    public void reloadForLanguage(){
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        Resources resources = getResources();
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+        recreate();
+    }
+     */
 
     public void openGroupPage(View v){
         Intent intent = new Intent(this, GroupPage.class);
@@ -31,8 +48,34 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void openMainSettings(View v){
+    /*public void openMainSettings(View v){
         Intent intent = new Intent(this, MainSettings.class);
         startActivity(intent);
     }
+     */
+
+    public void changeLanguageToNextInOrder(View view){
+        String language = getResources().getConfiguration().locale.getLanguage();
+        if (language == "de"){
+            language = "en";
+            System.out.println(language);
+        }
+        else if (language == "en"){
+            language = "fr";
+            System.out.println(language);
+        }
+        else if (language == "fr"){
+            language = "de";
+            System.out.println(language);
+        }
+        System.out.println(language);
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        Resources resources = getResources();
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+        recreate();
+    }
+
 }
--- app/src/main/java/com/example/trinkspiel/MainSettings.java
+++ app/src/main/java/com/example/trinkspiel/MainSettings.java
@@ -2,17 +2,42 @@ package com.example.trinkspiel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Locale;
 public class MainSettings extends AppCompatActivity {
 
-
+    private String language = "de";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_settings);
+    }
+
+    /*
+    public void reloadForLanguage(View view){
+        TextView tv = (TextView) view;
+        language = tv.getText().toString();
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        Resources resources = getResources();
+        resources.updateConfiguration(config, resources.getDisplayMetrics());
+        System.out.println(language);
+        recreate();
+    }
+     */
+
+    public void settingsMade(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        //intent.putExtra("changedLanguage", language);
+        startActivity(intent);
     }
 }
